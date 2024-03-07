3ebd1c3ec32b396d309c8947f5f4002ca2865d9a
khaniy <kangh1596@gmail.com>
2024-03-07 23:58:43 +0900

    [Docs] 3개의 정수 중 최댓값 구하기

---
+++ Book1/src/Chap1/Max3.java
@@ -0,0 +1,14 @@
+package Chap1;
+
+public class Max3 {
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        return max;
+    }
+}
