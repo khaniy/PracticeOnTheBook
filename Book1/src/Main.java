5c217c091e4931162adac323849fe0372eb2f07f
khaniy <kangh1596@gmail.com>
2024-03-10 00:01:20 +0900

    [Docs] copy reverse 함수 수정

--- Book1/src/Main.java
+++ Book1/src/Main.java
@@ -1,5 +1,16 @@
+import Chap2.ArrayFunction;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = new int[5];
+        ArrayFunction.copyReverse(a,b);
+        System.out.println(ArrayFunction.sumOf(a));
+        System.out.println(Arrays.toString(b));
     }
+
+
 }
