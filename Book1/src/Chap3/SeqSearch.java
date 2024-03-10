a0dea0e6d277be9bbfdc157cd2c626d8f5b34e33
khaniy <kangh1596@gmail.com>
2024-03-10 17:01:26 +0900

    [Docs] 선형검색 로직- 선형검색 보초법

--- Book1/src/Chap3/SeqSearch.java
+++ Book1/src/Chap3/SeqSearch.java
@@ -20,4 +20,16 @@ public class SeqSearch {
         }
         return -1;
     }
+
+    static int seqSearchSen(int[] dat, int n, int key){
+        int i=0;
+        dat[n] = key;
+        while(true){
+            if(dat[i]==key)
+                break;
+            i++;
+        }
+        return i==n ?-1 : i;
+    }
+
 }
