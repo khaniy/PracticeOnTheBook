d0f7067942a83dbd52819fe7e215d111bc59eabc
khaniy <kangh1596@gmail.com>
2024-03-20 23:51:07 +0900

    [Docs] Recursive - 하노이의 탑 문제 비재귀 함수로 구현

--- Book1/src/Chap5/Hanoi.java
+++ Book1/src/Chap5/Hanoi.java
@@ -1,14 +1,56 @@
 package Chap5;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Hanoi {
-    static void move(int no, int x, int y) {
+    static void originMove(int no, int x, int y) {
         if (no > 1)
-            move(no - 1, x, 6 - x - y);
+            originMove(no - 1, x, 6 - x - y);
         System.out.printf("원반 [%d]를 %d번 기둥에서 %d번 기둥으로 옮김\n", no, x, y);
         if (no > 1)
-            move(no - 1, 6 - x - y, y);
+            originMove(no - 1, 6 - x - y, y);
+    }
+
+    static void noRecurMove(int no, int x, int y){
+        int[] xstk = new int[100];
+        int[] ystk = new int[100];
+        int[] sstk = new int[100];		// 스택
+        int ptr = 0; 						// 스택포인터
+        int sw = 0;
+
+        while (true) {
+            if (sw == 0 && no > 1) {
+                xstk[ptr] = x;						// x의 값을 푸시
+                ystk[ptr] = y;						// y의 값을 푸시
+                sstk[ptr] = sw;						// sw의 값을 푸시
+                ptr++;
+                no = no - 1;
+                y = 6  - x - y;
+                continue;
+            }
+
+            System.out.printf("원반[%d]을 %d 기둥에서 %d 기둥으로 이동\n", no, x, y);
+
+            if (sw == 1 && no > 1) {
+                xstk[ptr] = x;						// x의 값을 푸시
+                ystk[ptr] = y;						// y의 값을 푸시
+                sstk[ptr] = sw;						// sw의 값을 푸시
+                ptr++;
+                no = no - 1;
+                x = 6  - x - y;
+                if (++sw == 2) sw = 0;
+                continue;
+            }
+            do {
+                if (ptr-- == 0) 					// 스택이 비어 있으면
+                    return;
+                x  = xstk[ptr]; 					// 값을 저장하고 있는 x를 팝
+                y  = ystk[ptr]; 					// 값을 저장하고 있는 y을 팝
+                sw = sstk[ptr] + 1;				// 값을 저장하고 있는 sw을 팝
+                no++;
+            } while (sw == 2);
+        }
     }
 
     public static void main(String[] args) {
@@ -16,6 +58,6 @@ public class Hanoi {
         System.out.println("하노이의 탑");
         System.out.println("원반의 개수: ");
         int n = sc.nextInt();
-        move(n, 1, 3);
+        originMove(n, 1, 3);
     }
 }
